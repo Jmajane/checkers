3a22c09882ce5a1c11a7e062b0570b17a84aebdb
Jmajane <jordan.majane@outlook.com>
2022-09-02 11:58:50 -0400

    checking if kind completed, add border to king

--- Main.js
+++ Main.js
@@ -181,7 +181,19 @@ function checkAvailableJumpSpaces() {
 // resricts movement if the piece is a king
 function checkPieceConditions() {
     if (selectedPiece.isKing) {
+        givePieceBorder();        
+    } else {
+        if (turn) {
+            selectedPiece.minusSeventhSpace = false;
+            selectedPiece.minusNinthSpace = false;
+            selectedPiece.minusFourteenthSpace = false;
+            selectedPiece.minusEighteenthSpace = false;  
+        } else {
+            selectedPiece.seventhSpace = false;
+            selectedPiece.ninthSpace = false;
+            selectedPiece.fourteenthSpace = false;
+            selectedPiece.eighteenthSpace = false;
+        }
         givePieceBorder();
-        
     }
 }
