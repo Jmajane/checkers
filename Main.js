51fb05f3c24fd9f42e65711f5d500b9803112eb8
Jmajane <jordan.majane@outlook.com>
2022-09-02 15:21:03 -0400

    black piece making moves setup

--- Main.js
+++ Main.js
@@ -246,5 +246,14 @@ function makeMove(number) {
             cells[selectedPiece.indexOfBoardPiece + number].innerHTML = `<p class="red-piece" id="${selectedPiece.pieceId}"></p>`; 
             redsPieces = document.querySelectorAll("p");
         }
+    } else {
+        if (selectedPiece.isKing) {
+            cells[selectedPiece.indexOfBoardPiece + number].innerHTML = `<spam class="black-piece king" id="${selectedPiece.pieceId}"></span>`; 
+            blacksPieces = document.querySelectorAll("span");
+        } else {
+            cells[selectedPiece.indexOfBoardPiece + number].innerHTML = `<span class="black-piece" id="${selectedPiece.pieceId}"></span>`; 
+            blacksPieces = document.querySelectorAll("span");
+        }
     }
+    
 }
